4efb0712f16e46f53e624d95cd054e89e3b55206
fodnb <Daniel.Mcghee6813@gmail.com>
2018-05-22 12:24:02 -0400

    Add autoscroll functionality to messages div

--- public/js/index.js
+++ public/js/index.js
@@ -1,5 +1,24 @@
 var socket = io();
 
+function scrollToBottom () {
+	//Selectors
+	var messages = $("#messages");
+	var newMessage = messages.children('li:last-child');
+
+	//Heights
+	var clientHeight = messages.prop('clientHeight');
+	var scrollTop = messages.prop('scrollTop');
+	var scrollHeight = messages.prop('scrollHeight');
+	var newMessageHeight = newMessage.innerHeight();
+	var lastMessageHeight = newMessage.prev().innerHeight();
+
+	if(clientHeight + scrollTop + newMessageHeight + lastMessageHeight >= scrollHeight){
+		messages.scrollTop(scrollHeight);
+		console.log('should scroll');
+	}
+};
+
+
 socket.on('connect', function(){
 	console.log('connected to server');
 });
@@ -10,26 +29,30 @@ socket.on('disconnect', function(){
 
 
 socket.on('newMessage', function(message){
-	var li = $('<li></li>');
-	var formattedTime = moment(message.createdAt).format('h:mm a');
-	li.text(`${message.from} ${formattedTime}: ${message.text}`);
-
-	$("#messages").append(li);
-
-
-	console.log('new message', message);
+	var template = $('#message-template').html();
+	var html = 	Mustache.render(template, {
+		text: message.text,
+		createdAt: moment(message.createdAt).format('h:mm a'),
+		from: message.from
+	});
+	$("#messages").append(html)
+	scrollToBottom();
+	// console.log('new message', message);
 });
 
 
 socket.on('newLocationMessage', function(message){
 	var formattedTime = moment(message.createdAt).format('h:mm a');
-	var li = $('<li></li>');
-	var a = $('<a target="_blank">My current location.<a/>')
-	li.text(`${message.from} ${formattedTime}: `);
-	a.attr('href', message.url);
-	li.append(a);
-	$("#messages").append(li);
+	var template = $('#location-message-template').html();
+	var html = Mustache.render(template, {
+		from: message.from,
+		text: message.text,
+		createdAt: formattedTime,
+		url: message.url
+	});
 
+	$("#messages").append(html);
+	scrollToBottom();
 });
 		
 $("#message-form").on('submit', function(e){
